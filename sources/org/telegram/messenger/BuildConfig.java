4b438ddd67582b294735735aca2239a3e1c44f34
FabianPastor <fabian_pastor@msn.com>
2019-12-08 09:46:50 +0100

    jadx-0.9.0 - B2 v5.12.0 (1753)

--- sources/org/telegram/messenger/BuildConfig.java
+++ sources/org/telegram/messenger/BuildConfig.java
@@ -5,6 +5,6 @@ public final class BuildConfig {
     public static final String BUILD_TYPE = "HA";
     public static final boolean DEBUG = false;
     public static final String FLAVOR = "afat";
-    public static final int VERSION_CODE = 1752;
+    public static final int VERSION_CODE = 1753;
     public static final String VERSION_NAME = "5.12.0";
 }
