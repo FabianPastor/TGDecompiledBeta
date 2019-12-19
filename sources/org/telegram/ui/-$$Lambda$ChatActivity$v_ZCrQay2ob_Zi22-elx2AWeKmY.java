e4dbcb586d2df7a4163ef499f2ff73884adba728
Fabian <fabian_pastor@msn.com>
2019-12-19 08:20:52 +0100

    jadx-0.9.0 - B2 v5.13.0 (1790)

---
+++ sources/org/telegram/ui/-$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY.java
@@ -0,0 +1,18 @@
+package org.telegram.ui;
+
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnShowListener;
+import android.widget.DatePicker;
+
+/* compiled from: lambda */
+public final /* synthetic */ class -$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY implements OnShowListener {
+    private final /* synthetic */ DatePicker f$0;
+
+    public /* synthetic */ -$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY(DatePicker datePicker) {
+        this.f$0 = datePicker;
+    }
+
+    public final void onShow(DialogInterface dialogInterface) {
+        ChatActivity.lambda$null$36(this.f$0, dialogInterface);
+    }
+}
