08fd246f0d8594319dde715830cc52ae400aecb9
Fabian <fabian_pastor@msn.com>
2020-01-05 15:58:02 +0100

    jadx-0.9.0 - B2 v5.13.0 (1790)

--- sources/org/telegram/ui/-$$Lambda$ChatActivity$6Nb1FRcfRrF4jNhtIay-Wm2eozQ.java
+++ sources/org/telegram/ui/-$$Lambda$ChatActivity$6Nb1FRcfRrF4jNhtIay-Wm2eozQ.java
@@ -4,14 +4,14 @@ import android.app.DatePickerDialog.OnDateSetListener;
 import android.widget.DatePicker;
 
 /* compiled from: lambda */
-public final /* synthetic */ class -$$Lambda$ChatActivity$xWY9Zk1XOWHRjQSFnxw30OLOHfY implements OnDateSetListener {
+public final /* synthetic */ class -$$Lambda$ChatActivity$6Nb1FRcfRrF4jNhtIay-Wm2eozQ implements OnDateSetListener {
     private final /* synthetic */ ChatActivity f$0;
 
-    public /* synthetic */ -$$Lambda$ChatActivity$xWY9Zk1XOWHRjQSFnxw30OLOHfY(ChatActivity chatActivity) {
+    public /* synthetic */ -$$Lambda$ChatActivity$6Nb1FRcfRrF4jNhtIay-Wm2eozQ(ChatActivity chatActivity) {
         this.f$0 = chatActivity;
     }
 
     public final void onDateSet(DatePicker datePicker, int i, int i2, int i3) {
-        this.f$0.lambda$null$33$ChatActivity(datePicker, i, i2, i3);
+        this.f$0.lambda$null$34$ChatActivity(datePicker, i, i2, i3);
     }
 }
--- sources/org/telegram/ui/-$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY.java
+++ sources/org/telegram/ui/-$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY.java
@@ -5,14 +5,14 @@ import android.content.DialogInterface.OnShowListener;
 import android.widget.DatePicker;
 
 /* compiled from: lambda */
-public final /* synthetic */ class -$$Lambda$ChatActivity$vuAgj-EWqg7NCJxCjtKTVkc-eH8 implements OnShowListener {
+public final /* synthetic */ class -$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY implements OnShowListener {
     private final /* synthetic */ DatePicker f$0;
 
-    public /* synthetic */ -$$Lambda$ChatActivity$vuAgj-EWqg7NCJxCjtKTVkc-eH8(DatePicker datePicker) {
+    public /* synthetic */ -$$Lambda$ChatActivity$v_ZCrQay2ob_Zi22-elx2AWeKmY(DatePicker datePicker) {
         this.f$0 = datePicker;
     }
 
     public final void onShow(DialogInterface dialogInterface) {
-        ChatActivity.lambda$null$35(this.f$0, dialogInterface);
+        ChatActivity.lambda$null$36(this.f$0, dialogInterface);
     }
 }
